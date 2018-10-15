c4f318065c75efe81563a3021735201288be59f4
Karan Sangha <iamkaransangha@gmail.com>
2018-10-15 10:36:44 -0700

    Initial Commit

---
+++ insomnia/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  insomnia
+//
+//  Created by Karan Sangha on 2018-10-15.
+//  Copyright © 2018 Karan Sangha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ insomnia/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  insomnia
+//
+//  Created by Karan Sangha on 2018-10-15.
+//  Copyright © 2018 Karan Sangha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
